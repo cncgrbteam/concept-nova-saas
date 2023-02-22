e702924fbfbf50cf96af8366ba3afd841547aae2
Ezichi Ebere <tripleee@yahoo.co.uk>
2023-02-22 13:17:32 +0100

    Changed homepage.

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Concept Nova SAAS</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css" rel="stylesheet" />
+</head>
+<body class="flex flex-col justify-center items-center min-h-screen bg-gray-100">
+    <h1 class="text-3xl font-bold text-blue-800">Concept Nova SAAS</h1>
+    <div class="flex my-10 p-5 w-full md:w-1/2">
+        <div class="w-1/2 p-2 flex justify-center items-center">
+            <div class="flex justify-center items-center w-44 h-44 border border-blue-800 hover:shadow-md">
+                <a href="#">ProDrive</a>
+            </div>
+        </div>
+        <div class="w-1/2 p-2 flex justify-center items-center">
+            <div class="flex justify-center items-center w-44 h-44 border border-blue-800 hover:shadow-md">
+                <a href="#">FMA</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
