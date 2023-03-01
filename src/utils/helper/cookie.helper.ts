5f11c9774844ab17c82a6373873867fda4829618
cradoe <noblewapinfo@gmail.com>
2023-03-01 16:32:36 +0100

    Updated expiry date for cookie

--- src/utils/helper/cookie.helper.ts
+++ src/utils/helper/cookie.helper.ts
@@ -1,33 +1,34 @@
-export const setCookie = (cname: string, cvalue: string, exdays: number) => {
-	const d = new Date();
-	d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
-	let expires = "expires=" + d.toUTCString();
-	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+export const setCookie = (cname: string, cvalue: string) => {
+  const d = new Date();
+
+  d.setTime(d.getTime() + 3 * 60 * 60 * 1000); // 3 hours
+  let expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 };
 
 export const getCookie = (name: string, req?: any): string | null => {
-	if (typeof document !== "undefined") {
-		// for client side rendering
-		const cookies = document.cookie.split(";");
-		for (let i = 0; i < cookies.length; i++) {
-			const cookie = cookies[i].trim();
-			if (cookie.startsWith(`${name}=`)) {
-				return cookie.substring(name.length + 1);
-			}
-		}
-	} else if (req?.headers?.cookie) {
-		// for server side rendering
-		const cookies = req.headers.cookie.split(";");
-		for (let i = 0; i < cookies.length; i++) {
-			const cookie = cookies[i].trim();
-			if (cookie.startsWith(`${name}=`)) {
-				return cookie.substring(name.length + 1);
-			}
-		}
-	}
-	return null;
+  if (typeof document !== "undefined") {
+    // for client side rendering
+    const cookies = document.cookie.split(";");
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      if (cookie.startsWith(`${name}=`)) {
+        return cookie.substring(name.length + 1);
+      }
+    }
+  } else if (req?.headers?.cookie) {
+    // for server side rendering
+    const cookies = req.headers.cookie.split(";");
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      if (cookie.startsWith(`${name}=`)) {
+        return cookie.substring(name.length + 1);
+      }
+    }
+  }
+  return null;
 };
 
 export const deleteCookie = (cname: string) => {
-	document.cookie = cname + "=" + "" + ";max-age=0path=/";
+  document.cookie = cname + "=" + "" + ";max-age=0path=/";
 };
