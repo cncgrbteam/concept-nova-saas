a90259dfae3c63db9f55ba166882911a21219917
cradoe <noblewapinfo@gmail.com>
2023-03-06 11:30:15 +0100

    Added button variant type

--- src/components/common/button/Button.type.ts
+++ src/components/common/button/Button.type.ts
@@ -1,13 +1,13 @@
 import { ButtonVariant } from "@utils";
 
 export type ButtonProps = {
-	onClick?: () => void;
-	type?: "button" | "submit" | "reset";
-	variant?: ButtonVariant;
-	disabled?: boolean;
-	isLoading?: boolean;
-	children?: React.ReactNode;
-	as?: "button" | "link";
-	href?: string;
-	className?: string;
+  onClick?: () => void;
+  type?: "button" | "submit" | "reset";
+  variant?: ButtonVariant;
+  disabled?: boolean;
+  isLoading?: boolean;
+  children?: React.ReactNode;
+  as?: "button" | "link";
+  href?: string;
+  className?: string;
 };
