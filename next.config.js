345eb5d87f35800c8873610971696995987e50bd
cradoe <noblewapinfo@gmail.com>
2023-03-07 15:14:33 +0100

    Added basePath and publicPath

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: "/sso-landing",
+  publicPath: "/sso-landing/_next/",
 };
 
 module.exports = nextConfig;
